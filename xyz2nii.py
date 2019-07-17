e4269fe7637660b498f91073f198dcd6940323fe
mnc1423 <41675771+mnc1423@users.noreply.github.com>
2019-07-17 15:18:47 +0900

    Create xyz2nii.py

---
+++ xyz2nii.py
@@ -0,0 +1,60 @@
+def xyz2nii(input_xyz_file, output_nii_file='', origin=[], pad=10):
+    """
+    Convert [x,y,z] coordinate file to nifti (nii.gz) volume file.
+    Parameters
+    ----------
+    input_xyz_file : string
+        input [x,y,z] coordinate text file
+    output_nii_file : string
+        output nifti (nii.gz) volume file
+    origin : list of floats
+        [x,y,z] coordinates for origin
+    pad : integer
+        number of voxels to pad input coordinates in x, y, and z directions
+    Returns
+    -------
+    output_nii_file : string
+        output nifti (nii.gz) volume file
+    Examples
+    --------
+    >>> from mindboggle.mio.convert_volumes import xyz2nii
+    >>> input_xyz_file = 'face.xyz.txt' # doctest: +SKIP
+    >>> origin = []
+    >>> pad = 10
+    >>> output_nii_file = 'xyz2nii.nii.gz'
+    >>> xyz2nii(input_xyz_file) # doctest: +SKIP
+    
+    
+    
+    View nifti file (skip test):
+    >>> from mindboggle.mio.plots import plot_volumes
+    >>> plot_volumes('xyz.nii.gz') # doctest: +SKIP
+    """
+    import os
+    import numpy as np
+    import nibabel as nb
+
+    # Load coordinates and scalars:
+    XYZscalars = np.loadtxt(input_xyz_file)
+    XYZ = np.round(XYZscalars[:, 0:3])
+    #scalars = XYZscalars[:, 3::]
+
+    if origin:
+        XYZ -= origin
+
+    XYZ += np.abs(np.min(XYZ, axis=0)) + [pad, pad, pad]
+    XYZ = np.round(XYZ)
+    dims = np.max(XYZ, axis=0) + [pad, pad, pad]
+    data = np.zeros(dims)
+
+    # Loop through rows or array and write 1s in image volume:
+    for irow, xyz in enumerate(XYZ):
+        data[xyz[0], xyz[1], xyz[2]] = 1
+
+    # Write output image volume:
+    if not output_nii_file:
+        output_nii_file = os.path.join(os.getcwd(), 'xyz.nii.gz')
+    img = nb.Nifti1Image(data, affine=np.eye(4,4))
+    img.to_filename(output_nii_file)
+
+    return output_nii_file
